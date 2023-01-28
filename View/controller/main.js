8ec46b926462b0e5fee1acc0ff37d0425498c9d4
RyanLouw <110169633+RyanLouw@users.noreply.github.com>
2023-01-28 14:59:08 +0200

    bata van die js
    Die hoof finkies van die ja is in gesit
    Seartch en sort

---
+++ View/controller/main.js
@@ -0,0 +1,100 @@
+fetch('./bikes_response.json')
+.then(response => response.json())
+.then(data => {
+let allData = data;
+//code to generate the view
+data.forEach(bike => {
+let view = document.getElementById("view");
+let div = document.createElement("div");
+div.classList.add("bike-card");
+div.innerHTML = `
+                        <h2>${bike.Make} ${bike.Model}</h2>
+                        <p>Year: ${bike.Year}</p>
+                        <p>Displacement: ${bike.Displacement}cc</p>
+                        <p>Price: ${bike.Price}</p>
+                        <p>Terrain: ${bike.Terrain}</p>
+                        <p>Description: ${bike.Description}</p>`;
+                        view.appendChild(div);
+});
+
+
+
+// Add event listener for the search button
+document.getElementById("search-button").addEventListener("click", function(){
+let searchValue = document.getElementById("make-search").value;
+let matchingBikes = data.filter(bike => bike.Make.toLowerCase() === searchValue.toLowerCase());
+// clear the current view
+view.innerHTML = "";
+// generate the view for the matching bikes
+matchingBikes.forEach(bike => {
+let div = document.createElement("div");
+div.classList.add("bike-card");
+div.innerHTML = `
+                        <h2>${bike.Make} ${bike.Model}</h2>
+                        <p>Year: ${bike.Year}</p>
+                        <p>Displacement: ${bike.Displacement}cc</p>
+                        <p>Price: ${bike.Price}</p>
+                        <p>Terrain: ${bike.Terrain}</p>
+                        <p>Description: ${bike.Description}</p>`;
+view.appendChild(div);
+});
+});
+// Add event listener for the clear button
+document.getElementById("clear-button").addEventListener("click", function(){
+// clear the current view
+view.innerHTML = "";
+// generate the view for all the data
+allData.forEach(bike => {
+let div = document.createElement("div");
+div.classList.add("bike-card");
+div.innerHTML = `
+                        <h2>${bike.Make} ${bike.Model}</h2>
+                        <p>Year: ${bike.Year}</p>
+                        <p>Displacement: ${bike.Displacement}cc</p>
+                        <p>Price: ${bike.Price}</p>
+                        <p>Terrain: ${bike.Terrain}</p>
+                        <p>Description: ${bike.Description}</p>`;
+view.appendChild(div);
+});
+});
+
+
+
+// Function to sort the JSON data by a given property
+function sortBikes(property) {
+    allData.sort(function(a, b) {
+      let x = a[property];
+      let y = b[property];
+      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
+
+
+  // Add an event listener to the select element
+  document.getElementById("sort-select").addEventListener("change", function() {
+    let selectedOption = this.value;
+
+    // Clear the current view
+    let view = document.getElementById("view");
+    while (view.firstChild) {
+      view.removeChild(view.firstChild);
+    }
+
+    // Sort the JSON data by the selected property
+    sortBikes(selectedOption);
+
+    // Repopulate the view with the sorted JSON data
+    allData.forEach(bike => {
+      let div = document.createElement("div");
+      div.classList.add("bike-card");
+      div.innerHTML = `
+                        <h2>${bike.Make} ${bike.Model}</h2>
+                        <p>Year: ${bike.Year}</p>
+                        <p>Displacement: ${bike.Displacement}cc</p>
+                        <p>Price: ${bike.Price}</p>
+                        <p>Terrain: ${bike.Terrain}</p>
+                        <p>Description: ${bike.Description}</p>`;
+      view.appendChild(div);
+    });
+  });
+});
